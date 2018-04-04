8bb9dc0db8a45c714d4bca4aaaddc8b34359abe0
Chris Bailey <chris@chrisbailey.io>
2018-04-03 19:08:06 -0800

    Fix rectangle

--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -8,16 +8,18 @@
 Rectangle::Rectangle(double width, double height) {
 	_width = width;
 	_height = height;
+	_x = 0;
+	_y = 0;
 }
 
 std::string Rectangle::drawCurrentAboveToString() {
 	std::stringstream result;
 
-	result << psHeader(getX(), getY());
-	result << psLine(getWidth(), 0);
-	result << psLine(getWidth(), getHeight());
-	result << psLine(getHeight(), 0);
-	result << psLine(0, 0);
+	result << psHeader(getX() * getUnits(), getY() * getUnits());
+	result << psMove(0, 0);
+	result << psLine(getWidth() * getUnits(), 0);
+	result << psLine(getWidth() * getUnits(), getHeight() * getUnits());
+	result << psLine(0, getHeight() * getUnits());
 	result << psFooter();
 
 	return result.str();
